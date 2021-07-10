050ebf35608be347b2e26f03db1c23b6750f530a
camiloBlanco <mail@camiloblanco.com>
2021-07-10 15:21:47 +0100

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { LayoutPublicModule } from './layouts/layout-public/layout-public.module';
+import { LayoutPrivateModule } from './layouts/layout-private/layout-private.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FlexLayoutModule ],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    FlexLayoutModule, 
+    LayoutPublicModule,
+    LayoutPrivateModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
